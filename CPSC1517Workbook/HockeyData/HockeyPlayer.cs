8e44b6b4f1a7a55b69a79bf480ca96bdfcc0b161
Vanessa Sueppmayer <vanessa.sueppmayer@gmail.com>
2023-09-12 15:40:40 -0600

    HockeyPlayer class & Enum classes added

---
+++ CPSC1517Workbook/HockeyData/HockeyPlayer.cs
@@ -0,0 +1,21 @@
+﻿namespace Hockey.Data
+{
+    public class HockeyPlayer
+    {
+        //data fields
+        private string _firstName;  //use "string?" allows nulls
+        private string _lastName;
+        private string _birthPlace;
+        private DateOnly _dateOfBirth;
+        private int _heightInInches;
+        private int _weightInPounds;
+
+        private Position _positions;
+        private Shot _shot;
+
+
+        //properties
+
+        //constructors
+    }
+}
---
+++ CPSC1517Workbook/HockeyData/Shot.cs
@@ -0,0 +1,11 @@
+﻿namespace Hockey.Data
+{
+    /// <summary>
+    /// Represents the direction of the shot
+    /// </summary>
+    public enum Shot
+    {
+        Left = 1,
+        Right,
+    }
+}
