ff8dd7945c28bd3f56987eaa6304956595906305
Vanessa Sueppmayer <vanessa.sueppmayer@gmail.com>
2023-09-14 02:18:36 -0600

    complete class

--- CPSC1517Workbook/HockeyData/HockeyPlayer.cs
+++ CPSC1517Workbook/HockeyData/HockeyPlayer.cs
@@ -3,19 +3,178 @@
     public class HockeyPlayer
     {
         //data fields
-        private string _firstName;  //use "string?" allows nulls
+        private string _firstName;  //using "string?" allows nulls
         private string _lastName;
         private string _birthPlace;
         private DateOnly _dateOfBirth;
         private int _heightInInches;
         private int _weightInPounds;
 
-        private Position _positions;
-        private Shot _shot;
-
+        //we don't need the following
+        //private Position _positions;
+        //private Shot _shot;
 
         //properties
 
+        /// <summary>
+        /// Gets/Sets the Birthplace of a hockey player
+        /// </summary>
+        public string BirthPlace
+        {
+            get
+            {
+                return _birthPlace;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) //checks for null/empty/whitespace
+                {
+                    throw new ArgumentException("Birthplace cannot be null or empty.");
+                }
+                //if we get here, then no exception happened
+                _birthPlace = value;
+            }
+        }
+
+        /// <summary>
+        /// gets/sets the first name of a hockey player
+        /// </summary>
+        public string FirstName
+        {
+            get 
+            { 
+                return _firstName; 
+            } 
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("First name cannot be empty");
+                }
+                _firstName = value;
+            }
+        }
+
+        /// <summary>
+        /// gets/sets the last name of a hockey player
+        /// </summary>
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Last name cannot be empty or null");
+                }
+                _lastName = value;
+            }
+        }
+
+        /// <summary>
+        /// gets/sets the height of a hockey player in inches
+        /// </summary>
+        public int HeightInInches
+        {
+            get
+            {
+                return _heightInInches;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Weight must be more than 0");
+                }
+                _heightInInches = value;
+            }
+        }
+
+        /// <summary>
+        /// gets/ sets the weight of a hockey player in pounds
+        /// </summary>
+        public int WeightInPounds
+        {
+            get
+            {
+                return _weightInPounds;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("weight must be more than 0");
+                }
+                _weightInPounds = value;
+            }
+        }
+
+        /// <summary>
+        /// gets/sets the date of birth of a hockey player
+        /// </summary>
+        public DateOnly DateOfBirth
+        {
+            get
+            {
+                return _dateOfBirth;
+            }
+            set
+            {
+                DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
+                if(value >=  currentDate)
+                {
+                    throw new ArgumentException("Date cannot be a future or todays date.");
+                }
+                _dateOfBirth = value;
+            }
+        }
+
+        //Auto-implemented property
+
+        /// <summary>
+        /// Represents the position of the hockey player
+        /// </summary>
+        public Position Position { get; set; }
+
+        /// <summary>
+        /// Represents the shot hand of the hockey player
+        /// </summary>
+        public Shot Shot { get; set; }
+
         //constructors
+
+        /// <summary>
+        /// creates the default instance of a hockey player
+        /// </summary>
+        public HockeyPlayer() //default constructor
+        {
+            _firstName = string.Empty;
+            _lastName = string.Empty;
+            _birthPlace = string.Empty;
+            _dateOfBirth = new DateOnly();
+            _weightInPounds = 0;
+            _heightInInches = 0;
+            Shot = Shot.Left;
+            Position = Position.Center;
+        }
+
+        //greedy constructor
+        public HockeyPlayer(string firstName, string lastname, string birthPlace, DateOnly dateOfBirth, int weightInPounds, int heightInInches, Position position, Shot shot)  
+            //order matters, defaults have to be at the end e.g.(Position position = Position.center, Shot shot = Shot.left)
+            //you can default parameters with a greedy constructor where you pass in the parameters
+        {
+            FirstName = firstName;
+            LastName = lastname;
+            BirthPlace = birthPlace;
+            DateOfBirth = dateOfBirth;
+            WeightInPounds = weightInPounds;
+            HeightInInches = heightInInches;
+            Position = position;
+            Shot = shot;
+        }
+
     }
 }
